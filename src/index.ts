c93dd4d2f3bc5cca77ed83820c3f496b0ca2822e
wirdana-B <wirdanadana88@gmail.com>
2026-07-09 15:12:20 +0700

    Set default route to login page

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import { db } from "./db";
 import { users } from "./db/schema";
 
 const app = new Elysia()
-  .get("/", () => "Hello World")
+  .get("/", () => Bun.file("src/frontend/login.html"))
+  .get("/login", () => Bun.file("src/frontend/login.html"))
+  .get("/login.css", () => Bun.file("src/frontend/login.css"))
   .get("/users", async () => {
     return await db.select().from(users);
   })
